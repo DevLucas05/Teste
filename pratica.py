2a7360c9bc694bbac8c3ff3e6b45a4092567eaf7
Lucas Nascimento <lucasnascimento05.dev@gmail.com>
2024-09-02 19:58:17 -0300

    primeiro commit

---
+++ pratica.py
@@ -0,0 +1,22 @@
+#nome = ('Lucas')
+#idade = (19)
+
+#print(f'Meu nome é {nome} e tenho {idade} anos.')
+#print(""" 
+ #     A 
+ #     L
+  #    U
+   #   R
+ #     A
+    #  """)
+
+pi = 3.14159
+
+# Abordagem de f-string
+print(f'O valor arredondado de pi é: {pi:.2f}')
+
+# Abordagem de .format()
+print('O valor arredondado de pi é: {:.2f}'.format(pi))
+
+# Utilizando a função round()
+print('O valor arredondado de pi é:', round(pi, 2))
